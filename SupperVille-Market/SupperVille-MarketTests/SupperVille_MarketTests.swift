c7154b7d77e737412eca0f770db70406eb110d0f
Vitor Spessoto <spessoto97@icloud.com>
2025-01-10 21:01:33 -0300

    Xcode init

---
+++ SupperVille-Market/SupperVille-MarketTests/SupperVille_MarketTests.swift
@@ -0,0 +1,17 @@
+//
+//  SupperVille_MarketTests.swift
+//  SupperVille-MarketTests
+//
+//  Created by Vitor Spessoto on 10/01/25.
+//
+
+import Testing
+@testable import SupperVille_Market
+
+struct SupperVille_MarketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
